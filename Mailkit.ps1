d7ba40154e29bae6a45b63283614eef5baa247ec
Compukid <Compukid377@Gmail.com>
2021-08-25 16:38:36 -0400

    uploaded files
    first draft, still need to test.

---
+++ Mailkit.ps1
@@ -0,0 +1,70 @@
+﻿#code came from website
+#https://adamtheautomator.com/powershell-email/
+#slightly modified
+
+Function Send-MailkitMessage {
+  [CmdletBinding(
+      SupportsShouldProcess = $true,
+      ConfirmImpact = "Low"
+  )] # Terminate CmdletBinding
+
+  Param(
+    [Parameter( Position = 0, Mandatory = $True )][String]$To,
+    [Parameter( Position = 1, Mandatory = $True )][String]$Subject,
+    [Parameter( Position = 2, Mandatory = $True )][String]$Body,
+    [Parameter( Position = 3 )][Alias("ComputerName")][String]$SmtpServer = $PSEmailServer,
+    [Parameter( Mandatory = $True )][String]$From,
+    [String]$CC,
+    [String]$BCC,
+    [Switch]$BodyAsHtml,
+    [switch]$Secure,
+    $Credential,
+    [Int32]$Port = 25
+  )
+
+  begin{
+    
+    Add-Type -Path "C:\Program Files\PackageManagement\NuGet\Packages\MailKit.2.15.0\lib\net47\MailKit.dll"
+    Add-Type -Path "C:\Program Files\PackageManagement\NuGet\Packages\MimeKit.2.15.0\lib\net47\MimeKit.dll" 
+  }
+
+  Process {
+    $SMTP     = New-Object MailKit.Net.Smtp.SmtpClient
+    $Message  = New-Object MimeKit.MimeMessage
+
+    If ($BodyAsHtml) {
+      $TextPart = [MimeKit.TextPart]::new("html")
+    } Else {
+      $TextPart = [MimeKit.TextPart]::new("plain")
+    }
+    
+    $TextPart.Text = $Body
+
+    $Message.From.Add($From)
+    $Message.To.Add($To)
+    
+    If ($CC) {
+      $Message.CC.Add($CC)
+    }
+    
+    If ($BCC) {
+      $Message.BCC.Add($BCC)
+    }
+
+    $Message.Subject = $Subject
+    $Message.Body    = $TextPart
+
+    if ($Secure) {$SMTP.Connect($SmtpServer, $Port,[MailKit.Security.SecureSocketOptions]::StartTls, $False)} else {$SMTP.Connect($SmtpServer, $Port, $False)}
+
+    If ($Credential) {
+      $SMTP.Authenticate($Credential.UserName, $Credential.GetNetworkCredential().Password)
+    }
+
+    If ($PSCmdlet.ShouldProcess('Send the mail message via MailKit.')) {
+      $SMTP.Send($Message)
+    }
+
+    $SMTP.Disconnect($true)
+    $SMTP.Dispose()
+  }
+}
---
+++ install_Mailkit.ps1
@@ -0,0 +1,17 @@
+﻿
+if ( (get-package -name mailkit) -eq $null) {
+
+    write-host "mailkit not installed" 
+
+    #Confirm nuget repository is added
+    if ( (Get-PackageSource -providername NuGet) -eq $null) {  
+        #Set Tls security and add nuget repository
+        [net.servicepointmanager]::SecurityProtocol = [net.securityprotocoltype]::Tls12
+        Register-PackageSource -Name MyNuGet -Location https://www.nuget.org/api/v2 -ProviderName NuGet
+    }
+
+    #install mimekit (depenecy) and mailkit. Need the SkipDependencies switch to bypass an error. By installing both apps it will meet requirments
+    Install-Package mimekit -ProviderName NuGet -SkipDependencies
+    Install-Package mailkit -ProviderName NuGet -SkipDependencies
+
+}
